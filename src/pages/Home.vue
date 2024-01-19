e7cbbd030e271f66225a3e1f4e6a83b1ce541b2a
Bastian Soto Iturra <64429095+Scarias@users.noreply.github.com>
2024-01-19 09:01:32 -0300

    responsiveness added in landing page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -18,17 +18,18 @@ onMounted(() => articleStore.queryArticles({
 <template>
     <section class="grid grid-rows-12 grid-cols-1 h-screen">
         <nav-bar title="Neering" />
-        <div class="flex flex-col max-w-4xl w-full gap-12 p-12 justify-self-center row-span-11">
-            <h1 class="text-dark text-4xl font-semibold">Bienvenido/a, {{ userStore.user.name }}</h1>
+        <div class="flex flex-col max-w-4xl w-full gap-6 sm:gap-12 p-6 sm:p-12 justify-self-center row-span-11">
+            <h1 class="text-dark text-3xl sm:text-4xl font-semibold">Bienvenido/a, {{ userStore.user.name }}</h1>
             <div class="flex justify-center items-center gap-4">
                 <button
                     class="text-xl px-4 py-2 rounded bg-secondary text-white active:bg-secondary/75"
                     type="button" @click="() => router.push('/articles')">
                     Ver todos los artículos
                 </button>
+                <button class="fixed"></button>
             </div>
             <div class="flex flex-col p-6 gap-4 shadow rounded bg-background">
-                <h2 class="text-dark text-3xl">Tus últimos artículos</h2>
+                <h2 class="text-dark text-2xl sm:text-3xl">Tus últimos artículos</h2>
                 <div
                     v-for="article in articleStore.articles.slice(0,4)"
                     class="p-4 rounded border-2 border-dark/10"
